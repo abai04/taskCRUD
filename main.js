f348e2297d7566b369c2e19061905d72f7ff6855
Abai <abaiesen04@gmail.com>
2023-10-02 17:20:52 +0600

    final

--- main.js
+++ main.js
@@ -18,9 +18,7 @@ saveBtn.addEventListener("click", () =>{
 })
 readBtn.addEventListener("click", () =>{
     createContact()
-    document.querySelectorAll("li").forEach(el=>{
-            el.after(document.createElement("hr"))
-        })
+    
 })
 
 function setToLocalStorage(contact){
@@ -37,20 +35,37 @@ function createContact(){
 
     data.forEach((el, i) =>{
         let li  =document.createElement("li")
-        li.innerHTML = `<img src="${el.image}" alt="" srcset="">
-            <div style="margin-right: 10px" class="info">
-                <p>${el.name}</p>
-                <p>${el.email}</p>
-                <p>${el.phone}</p>
-            </div>
-            <div><button class = "update-btn" style = "padding: 0px 10px; height: 20px">update</button>
-            <button class = "delete-btn" style = "padding: 0px 10px; height: 20px">delete</button>
-            </div>                    
-            `
+        let img = document.createElement("img")
+        img.setAttribute("src", el.image)
+        let pName = document.createElement("p")
+        pName.innerText = el.name
+        let pEmail = document.createElement("p")
+        pEmail.innerText = el.email    
+        let pPhone = document.createElement("p")  
+        pPhone.innerText = el.phone 
+        let infoBlock = document.createElement("div")
+        infoBlock.append(pName)
+        infoBlock.append(pEmail)
+        infoBlock.append(pPhone)
+        infoBlock.style.marginRight = "10px"
+        let buttonBlock = document.createElement("div")
+        let updateBtn = document.createElement("button")
+        let deleteBtn = document.createElement("button")
+        updateBtn.innerHTML = "update"
+        deleteBtn.innerHTML = "delete"
+        buttonBlock.append(updateBtn)
+        buttonBlock.append(deleteBtn)
+        li.append(img)
+        li.append(infoBlock)
+        li.append(buttonBlock)
         list.append(li) 
-        let btnDelete = document.querySelector(".delete-btn")
-        console.log(btnDelete);
-        btnDelete.onclick = () => deleteContact(i)              
+        deleteBtn.addEventListener("click", ()=>{
+            deleteContact(i)
+        })
+        updateBtn.addEventListener("click", ()=>{
+            editContact(i)
+        })
+        li.after(document.createElement("hr"))
     })
 }
 function deleteContact(index){
@@ -59,3 +74,36 @@ function deleteContact(index){
     localStorage.setItem("contact", JSON.stringify(data))
     createContact()
 }
+let modal = document.querySelector(".main-modal")
+let nameEdit = document.querySelector("#name-edit")
+let emailEdit = document.querySelector("#email-edit")
+let phoneEdit = document.querySelector("#phone-edit")
+let imgEdit = document.querySelector("#image-edit")
+let closer = document.querySelector(".btn-closer")
+let saveEdited = document.querySelector(".btn-save-edit")
+
+let idx = null
+function editContact(index){
+    modal.style.display = "block"
+    let data = JSON.parse(localStorage.getItem("contact"))
+    nameEdit.value = data[index].name
+    emailEdit.value = data[index].email
+    phoneEdit.value = data[index].phone
+    imgEdit.value = data[index].image
+    idx = index
+}
+closer.addEventListener("click", () => {
+    modal.style.display = "none"
+})
+saveEdited.addEventListener("click", () =>{
+    let data = JSON.parse(localStorage.getItem("contact"))
+    if(!nameEdit.value.trim() || !emailEdit.value.trim() || !phoneEdit.value.trim() || !imgEdit.value.trim()){
+        alert("fill the field!")
+        return
+    }
+    let editedContact = {name: nameEdit.value, email: emailEdit.value, phone: phoneEdit.value, image: imgEdit.value}
+    data.splice(idx, 1, editedContact)
+    localStorage.setItem("contact", JSON.stringify(data))
+    createContact()
+    modal.style.display = "none"
+})
